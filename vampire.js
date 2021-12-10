18b761805676140aed896c594d432480d236f8a6
meteora277 <justin.s.diaz@gmail.com>
2021-12-10 16:33:34 -0500

    defined vampire and worked on some of its methods

--- vampire.js
+++ vampire.js
@@ -10,22 +10,36 @@ class Vampire {
 
   // Adds the vampire as an offspring of this vampire
   addOffspring(vampire) {
-
+    vampire.creator = this;
+    this.offspring.push(vampire);
   }
 
   // Returns the total number of vampires created by that vampire
   get numberOfOffspring() {
-
+    return this.offspring.length;
   }
 
   // Returns the number of vampires away from the original vampire this vampire is
   get numberOfVampiresFromOriginal() {
-
+    let counter = 0;
+    let currentVampire = this;
+    while (currentVampire.creator) {
+      currentVampire = currentVampire.creator
+      counter += 1;
+    }
+    return counter;
   }
 
   // Returns true if this vampire is more senior than the other vampire. (Who is closer to the original vampire)
   isMoreSeniorThan(vampire) {
+    const age = this.numberOfVampiresFromOriginal;
+    const otherVampAge = vampire.numberOfVampiresFromOriginal;
+    // console.log(age, otherVampAge);
 
+    if (age < otherVampAge) {
+      return true;
+    }
+    return false;
   }
 
   /** Stretch **/
